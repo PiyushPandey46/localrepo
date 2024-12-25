af344d4c90e132d30c3077df7909eecce2fa47a2
piyushpandey46 <piyush.kdpandey@gmail.com>
2024-12-25 22:01:47 +0530

    add new feature

--- x.c
+++ x.c
@@ -2,10 +2,9 @@
 int main(){
     int a,b,c;
     printf("enter the value of a and b");
-    scanf("%d%d",&a,&b)
+    scanf("%d%d",&a,&b);
     c=a/b;
     c=a*b;
- 
     printf("the addition of two numbers is %d",c);
     c=a-b;
     printf("thr subtraction of two numbers id %d",c);
