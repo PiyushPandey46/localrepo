a75f2df18dc25519dfcaa964ccef3122ec5ba0ca
piyushpandey46 <piyush.kdpandey@gmail.com>
2024-12-25 21:49:57 +0530

    multiply

--- x.c
+++ x.c
@@ -3,7 +3,7 @@ int main(){
     int a,b,c;
     printf("enter the value of a and b");
     scanf("%d%d",&a,&b);
-    c=a+b;
+    c=a*b;
     printf("the addition of two numbers is %d",c);
     c=a-b;
     printf("thr subtraction of two numbers id %d",c);
